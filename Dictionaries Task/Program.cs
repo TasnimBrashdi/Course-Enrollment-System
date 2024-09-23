540ab5296303f6ee1af702ecbbf6692dc9af0ee6
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-23 10:37:50 +0400

    Adding log in student function

--- Dictionaries Task/Program.cs
+++ Dictionaries Task/Program.cs
@@ -4,7 +4,7 @@
     {
         static List<(int AID, string Aname, string email, string pas)> Admin = new List<(int AID, string Aname, string email, string pas)>();
         Dictionary<string, HashSet<string>> Enrollment_System = new Dictionary<string, HashSet<string>>();
-        static List<(int SID,string Semail, string Spas)> Student = new List<(int SID, string Semail, string Spas)>();
+        static List<(int SID,string SName,string Semail, string Spas)> Student = new List<(int SID, string SName, string Semail, string Spas)>();
         static void Main(string[] args)
         {
 
@@ -95,6 +95,61 @@
 
                 if (flag != true)
                 { Console.WriteLine("Admin not found Or input invaild"); }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Erorr" + e.Message);
+
+            }
+
+
+
+
+        }
+        static void LoginStudent()
+        {
+
+            Console.WriteLine("Enter your Email");
+            string email = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                Console.WriteLine("Email cannot be empty.");
+                return;
+            }
+
+
+            bool flag = false;
+            try
+            {
+                for (int i = 0; i < Student.Count; i++)
+                {
+                    if (Student[i].Semail == email)
+                    {
+                        Console.WriteLine("Enter your Password");
+                        string pas = Console.ReadLine();
+                        if (string.IsNullOrWhiteSpace(pas))
+                        {
+                            Console.WriteLine("Password cannot be empty.");
+                            return;
+                        }
+                        if (Student[i].Spas == pas)
+                        {
+                            Console.WriteLine("re-enter password your Password");
+                            string pas2 = Console.ReadLine();
+                            if (pas == pas2)
+                            {
+                                Console.WriteLine("WELCOME: " + Student[i].SName);
+                                flag = true;
+
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (flag != true)
+                { Console.WriteLine("Student not found Or input invaild"); }
             }
 
             catch (Exception e)
