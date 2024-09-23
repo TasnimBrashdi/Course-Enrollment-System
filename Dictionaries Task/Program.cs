477422f9a24fdf9b5f47f74883d12352ca131192
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-23 10:32:38 +0400

    Adding Logi n Admin function

--- Dictionaries Task/Program.cs
+++ Dictionaries Task/Program.cs
@@ -24,7 +24,7 @@
                     {
                         case "A":
 
-
+                            LoginAdmin();
                             break;
 
                         case "B":
@@ -51,6 +51,61 @@
             {
                 Console.WriteLine("ERROR" + ex.Message);
             }
+        }
+        static void LoginAdmin()
+        {
+
+            Console.WriteLine("Enter your Email");
+            string email = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                Console.WriteLine("Email cannot be empty.");
+                return;
+            }
+          
+           
+            bool flag = false;
+            try
+            {
+                for (int i = 0; i < Admin.Count; i++)
+                {
+                    if (Admin[i].email == email)
+                    {
+                        Console.WriteLine("Enter your Password");
+                        string pas = Console.ReadLine();
+                        if (string.IsNullOrWhiteSpace(pas))
+                        {
+                            Console.WriteLine("Password cannot be empty.");
+                            return;
+                        }
+                        if (Admin[i].pas == pas)
+                        {
+                            Console.WriteLine("re-enter password your Password");
+                            string pas2 = Console.ReadLine();
+                            if (pas == pas2)
+                            {
+                                Console.WriteLine("WELCOME: " + Admin[i].Aname);
+                                flag = true;
+
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (flag != true)
+                { Console.WriteLine("Admin not found Or input invaild"); }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Erorr" + e.Message);
+
+            }
+
+
+
+
         }
     }
     }
