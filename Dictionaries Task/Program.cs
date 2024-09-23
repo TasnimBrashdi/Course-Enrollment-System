bc5d0decb1996ed55b682a8c6f84644d34ab701b
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-23 10:20:12 +0400

    Adding structure system

--- Dictionaries Task/Program.cs
+++ Dictionaries Task/Program.cs
@@ -2,9 +2,56 @@
 {
     internal class Program
     {
+        static List<(int AID, string Aname, string email, string pas)> Admin = new List<(int AID, string Aname, string email, string pas)>();
+        Dictionary<string, HashSet<string>> Enrollment_System = new Dictionary<string, HashSet<string>>();
+        static List<(int SID,string Semail, string Spas)> Student = new List<(int SID, string Semail, string Spas)>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            bool ExitFlag = false;
+           
+            try
+            {
+                do
+                {
+                    Console.WriteLine("\n   - - - -   Course Enrollment System   - - - -  ");
+                    Console.WriteLine("\n Choose: \n A- Log in Admin \n B- Log in Student  \n C- Log out ");
+
+
+                    string choice = Console.ReadLine()?.ToUpper();
+
+                    switch (choice)
+                    {
+                        case "A":
+
+
+                            break;
+
+                        case "B":
+                           
+
+                            break;
+                        case "C":
+
+                            ExitFlag = true;
+
+                            break;
+                    
+                        default:
+                            Console.WriteLine("Sorry your choice was wrong");
+                            break;
+
+                    }
+
+                 
+
+                } while (ExitFlag != true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR" + ex.Message);
+            }
         }
     }
-}
+    }
+
