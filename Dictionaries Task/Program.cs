ff1b53cad00e6e66a30c534fdf15d8d696dceb90
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-23 09:42:24 +0400

    Start commit

---
+++ Dictionaries Task/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dictionaries_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
